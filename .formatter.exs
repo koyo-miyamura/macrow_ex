81c795e432fd0fbd8a318e8cc25ad631998ee7c9
koyo-miyamura <nexus9071@gmail.com>
2023-03-05 20:25:18 +0900

    Add exported locals_without_parens

--- .formatter.exs
+++ .formatter.exs
@@ -3,6 +3,6 @@
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
   locals_without_parens: [rules: 2, macro_prefix: 1, macro_suffix: 1],
   export: [
-    locals_without_parens: [rules: 2]
+    locals_without_parens: [rules: 2, macro_prefix: 1, macro_suffix: 1]
   ]
 ]
